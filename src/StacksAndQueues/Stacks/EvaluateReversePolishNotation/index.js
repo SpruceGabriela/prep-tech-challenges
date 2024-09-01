ae2e32fbfcde2801450ca46360aca224371df51a
Gabriela Pinheiro <gabipinheiro38@gmail.com>
2024-09-01 20:44:17 -0300

    feat: implement solution for EvaluateReversePolishNotation

---
+++ src/StacksAndQueues/Stacks/EvaluateReversePolishNotation/index.js
@@ -0,0 +1,40 @@
+"use strict";
+function evalRPN(tokens) {
+    let stack = [];
+    let operators = ['+', '-', '*', '/'];
+    for (let i = 0; i < tokens.length; i++) {
+        if (!operators.includes(tokens[i])) {
+            stack.push(Number(tokens[i]));
+        }
+        else {
+            let result = 0;
+            let a = stack.pop();
+            let b = stack.pop();
+            if (a && b) {
+                if (tokens[i] === '+') {
+                    result = (a + b);
+                }
+                if (tokens[i] === '-') {
+                    result = (a - b);
+                }
+                if (tokens[i] === '*') {
+                    result = (a * b);
+                }
+                if (tokens[i] === '/') {
+                    result = b / a;
+                    if (result > 0) {
+                        result = Math.floor(result);
+                    }
+                    else {
+                        result = Math.ceil(result);
+                    }
+                }
+                stack.push(result);
+            }
+        }
+    }
+    return stack.pop();
+}
+;
+//Complexidade de tempo: O(n)
+//Complexidade de espaço: O(n)
