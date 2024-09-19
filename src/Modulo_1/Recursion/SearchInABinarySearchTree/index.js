4d5abc5700095813c7715ec1ad22e1105425d6bd
Gabriela Pinheiro <gabipinheiro38@gmail.com>
2024-09-19 19:44:23 -0300

    feat: implement SearchInABinarySearchTree example

---
+++ src/Modulo_1/Recursion/SearchInABinarySearchTree/index.js
@@ -0,0 +1,31 @@
+"use strict";
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+function searchBST(root, val) {
+    if (root === null)
+        return null;
+    if (root.val === val)
+        return root;
+    // numa arvore binaria de busca, todos os elementos à esquerda serão menores.
+    //Então se o val que buscamos for menor que root.val, 
+    //sabemos que a resposta vai estar sempre do lado esquerdo
+    //caso não, buscar no lado direito, que sempre será maior
+    if (val < root.val) {
+        return searchBST(root.left, val);
+    }
+    else {
+        return searchBST(root.right, val);
+    }
+}
+;
