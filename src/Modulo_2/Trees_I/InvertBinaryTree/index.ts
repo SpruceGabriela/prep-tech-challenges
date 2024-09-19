88a8125e6b87cfe093c59bcfda238db82055d1bb
Gabriela Pinheiro <gabipinheiro38@gmail.com>
2024-09-19 20:03:28 -0300

    feat: implement example InvertBinaryTree

---
+++ src/Modulo_2/Trees_I/InvertBinaryTree/index.ts
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function invertTree(root: TreeNode | null): TreeNode | null {
+  if(root === null) {
+      return null;
+  }
+
+  if(root.left !== null || root.right !== null) {
+      let temp: TreeNode | null = root.left;
+      root.left = root.right;
+      root.right = temp;
+  }
+
+  invertTree(root.left);
+  invertTree(root.right);
+
+  return root;
+};
+
+//caso exista left e right, criar uma variavel temporaria p/left e realizar o swap
+//chamar a função recursivamente para cada novo nó left e right
