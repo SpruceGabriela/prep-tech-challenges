ba5cdbdca64abb02ce94200a2d02cef5e9fb5157
Gabriela Pinheiro <gabipinheiro38@gmail.com>
2024-09-19 19:21:33 -0300

    fix: sub module folder

---
+++ src/Modulo_1/Sorting/SortArrayByParity/index.ts
@@ -0,0 +1,23 @@
+function sortArrayByParity(nums: number[]): number[] {
+  let newArr = new Array(nums.length);
+  let arrIndex = 0;
+
+  for(let i = 0; i < nums.length; i++) {
+      if(nums[i] % 2 === 0) {
+          newArr[arrIndex] = nums[i];
+          arrIndex++;
+      }
+  }
+
+  for(let i = 0; i < nums.length; i++) {
+      if(nums[i] % 2 === 1) {
+          newArr[arrIndex] = nums[i];
+          arrIndex++;
+      }
+  }
+
+  return newArr;
+};
+
+//tempo: O(n)
+//espaco: O(n)
