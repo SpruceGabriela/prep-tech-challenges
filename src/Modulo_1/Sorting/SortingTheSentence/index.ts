a4a23c2514b3783fddddfab6ac4b524457dc8b22
Gabriela Pinheiro <gabipinheiro38@gmail.com>
2024-09-19 19:23:08 -0300

    feat: implement SortingTheSentence

---
+++ src/Modulo_1/Sorting/SortingTheSentence/index.ts
@@ -0,0 +1,17 @@
+function sortSentence(s: string): string {
+  let sentenceArray = s.split(" ");
+
+  if(sentenceArray.length === 1) {
+      return s.substring(0, s.length - 1);
+  }
+
+  let newArr: string[] = new Array(sentenceArray.length);
+
+  for(let i = 0; i < sentenceArray.length; i++) {
+      let currentWord = sentenceArray[i];
+      let index = Number(currentWord[currentWord.length - 1]) -1; //-1 no final pq o JS inicia o array com idx 0
+      newArr[index] = currentWord.substring(0, currentWord.length - 1);
+  }
+
+  return newArr.join(" ");
+};
